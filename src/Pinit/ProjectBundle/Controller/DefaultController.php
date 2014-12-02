f413b98c734a1886b05937367d55b85b1ebbfa72
Stef <stef@3wsimple.com>
2014-12-02 15:24:15 -0500

    Added base bundle with 2 basic test routes.

---
+++ src/Pinit/ProjectBundle/Controller/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pinit\ProjectBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('PinitProjectBundle:Default:index.html.twig', array('name' => $name));
+    }
+    
+}
