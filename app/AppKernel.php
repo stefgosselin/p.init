39696c026613f7a16d42acb03d2768ab08fea7b0
Stef <stef@3wsimple.com>
2014-12-02 20:44:22 -0500

    Updated FOSUser configuration.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Pinit\ProjectBundle\PinitProjectBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Pinit\UserBundle\PinitUserBundle(),
        );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
