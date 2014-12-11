ae47017688cb22a2fbe0e383f09f93cc3389126a
Stephane Gosselin <stef@3wsimple.com>
2014-12-11 00:34:44 -0500

    Updated fixtures to work in new setup.

--- p.init/src/Pinit/PinitBundle/DependencyInjection/Configuration.php
+++ p.init/src/Pinit/PinitBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('pinit_user');
+        $rootNode = $treeBuilder->root('pinit_default');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
